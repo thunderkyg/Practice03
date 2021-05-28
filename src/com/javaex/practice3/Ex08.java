297b45e9b931606569f71f2f1fc17650bc0de53a
thunderkyg <thunderkyg@gmail.com>
2021-05-28 09:54:44 +0900

    Redone1

--- src/com/javaex/practice3/Ex08.java
+++ src/com/javaex/practice3/Ex08.java
@@ -3,11 +3,11 @@ package com.javaex.practice3;
 public class Ex08 {
 
 	public static void main(String[] args) {
-		
-		
-		for (int y = 1; y <= 10; y++) {
-			for (int x = 0; x < 10 ; x++) {
-				System.out.print(y + x + "	");
+
+		for (int a = 1; a <= 9; a++) {
+			for (int b = 2; b <= 9; b++) {
+				int sum = a*b;
+				System.out.print(b + "*"+ a + "=" + sum + "	");
 			}
 			System.out.println("");
 		}
--- src/com/javaex/practice3/Ex09.java
+++ src/com/javaex/practice3/Ex09.java
@@ -5,6 +5,12 @@ public class Ex09 {
 	public static void main(String[] args) {
 		
 		
+		for (int y = 1; y <= 10; y++) {
+			for (int x = 0; x < 10 ; x++) {
+				System.out.print(y + x + "	");
+			}
+			System.out.println("");
+		}
 
 	}
 
